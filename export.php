26efabfd39796fdeda3626353cf3e962de4a11b6
prathan <than2557@gmail.com>
2020-01-08 14:53:16 +0700

    seconput

--- export.php
+++ export.php
@@ -1,72 +1,47 @@
  <?php  
  //export.php  
+
+         require_once 'phpexcel-1.8\Classes\PHPExcel.php'; 
+         include 'phpexcel-1.8\Classes\PHPExcel\IOFactory.php';
+
  if(!empty($_FILES["excel_file"]))  
  {  
-      $connect = mysqli_connect("localhost", "root", "", "test");  
+ 	//print_r( $_FILES["excel_file"]);
+
       $file_array = explode(".", $_FILES["excel_file"]["name"]);  
       if($file_array[1] == "xls"|| $file_array[1]=="xlsx"||$file_array[1]=="csv")  
       {  
-           include("phpexcel-1.8\Classes\PHPExcel.php");  
-           $output = '';  
-           $output .= "  
-           <label class='text-success'>Data Inserted</label>  
-                <table class='table table-bordered'>  
-                     <tr>  
-                          <th>Customer Name</th>  
-                          <th>Address</th>  
-                          <th>City</th>  
-                          <th>Postal Code</th>  
-                          <th>Country</th>  
-                     </tr>  
-                     ";  
-           $object = PHPExcel_IOFactory::load($_FILES["excel_file"]["tmp_name"]);  
-           foreach($object->getWorksheetIterator() as $worksheet)  
-           {  
-                $highestRow = $worksheet->getHighestRow();  
-                for($row=2; $row<=$highestRow; $row++)  
-                {  
-                     $name = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(1, $row)->getValue());  
-                     $address = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(2, $row)->getValue());  
-                     $city = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(3, $row)->getValue());  
-                     $postal_code = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(4, $row)->getValue());  
-                     $country = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(5, $row)->getValue());  
+           
+  				
+           $tmp_data =$_FILES["excel_file"]["tmp_name"];
+          $inputFileType = PHPExcel_IOFactory::identify($tmp_data);  
+         $objReader = PHPExcel_IOFactory::createReader($inputFileType);  
+         $objReader->setReadDataOnly(true);  
 
-                      
-  				$sql= 'CREATE TABLE IF NOT EXISTS $object["excel_file"](  
+         $objPHPExcel = $objReader->load($tmp_data); //pathdiritory  :: load static
+          $output = ''; 
 
- 					 "CustomerID" int(11) NOT NULL AUTO_INCREMENT,  
-   					"CustomerName" varchar(250) NOT NULL,  
-  					"Address" text NOT NULL,  
- 					"City" varchar(250) NOT NULL,  
-   					"PostalCode" varchar(30) NOT NULL,  
-   					"Country" varchar(100) NOT NULL,  
-  					PRIMARY KEY ("CustomerID")  
- 		) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 '; 
- 		mysqli_query($connect, $sql);  
+          foreach ( $objPHPExcel->getWorksheetIterator() as $worksheet) {
+          	# code...
+          	 $highestRow = $worksheet->getHighestRow();  
+          	 $highestColumn = $worksheet->getHighestColumn(); 
+          	 $highestColumnIndex = PHPExcel_Cell::columnIndexFromString( $highestColumn); 
+          	 echo $highestColumnIndex;
 
-                     $query = "  
-                     INSERT INTO tbl_customer  
-                     (CustomerName, Address, City, PostalCode, Country)   
-                     VALUES ('".$name."', '".$address."', '".$city."', '".$postal_code."', '".$country."')  
-                     ";  
-                     mysqli_query($connect, $query);  
-                     $output .= '  
-                     <tr>  
-                          <td>'.$name.'</td>  
-                          <td>'.$address.'</td>  
-                          <td>'.$city.'</td>  
-                          <td>'.$postal_code.'</td>  
-                          <td>'.$country.'</td>  
-                     </tr>  
-                     ';  
-                }  
-           }  
-           $output .= '</table>';  
-           echo $output;  
-      }  
-      else  
-      {  
-           echo '<label class="text-danger">Invalid File</label>';  
-      }  
- }  
+
+          	 for($row=2;$row<=$highestRow;$row++){
+          	 		for($i=0;$row<=$highestRow;$i++){
+
+          	 		//$getdata = $worksheet->getCellByColumnAndRow($i, $row)->getValue();
+
+
+
+          	 		}
+          	 }
+          }
+          
+ }
+
+}
+
  ?>  
---
+++ submit_file.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+
+    <link href="/test_excel/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/test_excel/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+<div class="container" style="width: 1140px;background-color:   #66a3ff;">
+  <div class="container" style="width: 100%;">
+            <h2>เพิ่มข้อมูลการแจ้งเตือน</h2>
+       
+        </div>  
+      </div>
+
+<div class="container">
+    <form action="" class="form-horizontal"  role="form">
+        <div class="container" style="width: 100%;background-color: #D5A9CC;">
+        <div class="form-group">
+            <div class="col-sm-12">
+                <br>
+                <br>
+            <label class="control-label col-sm-2"> tokenname:</label>
+            <input type="text"  class=" form-control control-label col-sm-1" style="width: 200px;">
+            <label class="control-label col-sm-2"> กลุ่มไลน์:</label>
+            <input type="text"  class=" form-control control-label col-sm-1" style="width: 200px;">
+            </div>
+        </div>
+        <div class="form-group">
+        <div class="col-sm-12">
+            <label class="control-label col-sm-2">ชื่อตาราง :</label>
+            <input type="text"  class=" form-control control-label col-sm-1" style="width: 200px;">
+            <label class="control-label col-sm-2">เลือกไฟล์ :</label>
+            <input  type="file" name="upload" class=" form-control control-label col-sm-1" style="width: 200px;">
+        </div>
+
+        </div>
+
+      
+       
+            <div class="form-group">
+                <div class="col-sm-12">
+                <label for="dtp_input1" class="col-md-2 control-label">วันเวลาในการแจ้งเตือน:</label>
+                <div class="input-group date form_datetime col-md-4" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="dtp_input1">
+                    <input class="form-control col-sm-2" size="16" type="text" value="" style="width:200px;">
+                    <span class="input-group-addon col-sm-2" style="width:30px;"><span class="glyphicon glyphicon-remove"></span></span>
+					<span class="input-group-addon col-sm-3" style="width:30px;"><span class="glyphicon glyphicon-th"></span></span>
+                </div>
+				<input type="hidden" id="dtp_input1" value="" /><br/>
+            </div>
+            </div>
+        </div>
+    </form>
+</div>
+<br>
+<div class="form-group">
+<div class="col-sm-12">
+  <center> <label class="control-label">ตัวอย่างข้อมูล</label></center>
+ <table class="table table-bordered table-striped" id="myTable" style="width: 500px;height:400px ;border-radius: 100%;">
+  <thead>
+    <tr>
+      <th>รหัสสินค้า</th>
+      <th>ชื่อสินค้า</th>
+      <th>ชนิทสินค้า</th>
+      <th>แบรนด์สินค้า</th>
+      <th>จำนวนสินค้า</th>
+      <th>ราคา</th>
+      <th>รูป</th>
+      <th>แก้ไข</th>
+      <th>ลบ</th>
+    </tr>
+  </thead>
+  <tbody>
+
+
+    <tr>
+      <td>13210</td>
+      <td>polop</td>
+      <td>werwer</td>
+      <td>asdas/td>
+      <td>asdasd</td>
+        <td>asdaas</td>
+      <td>fasfas</td>
+      <td>asasf</td>
+      <td>asfasfas</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+
+<script type="text/javascript" src="/test_excel/jquery/jquery-1.8.3.min.js" charset="UTF-8"></script>
+<script type="text/javascript" src="/test_excel/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/test_excel/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+<script type="text/javascript" src="/test_excel/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="datatables.css"/>
+
+<script type="text/javascript" src="datatables.js"></script>
+
+<script type="text/javascript">
+	$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+</script>
+<script type="text/javascript">
+    $('.form_datetime').datetimepicker({
+        //language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		forceParse: 0,
+        showMeridian: 1
+    });
+	$('.form_date').datetimepicker({
+        language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		minView: 2,
+		forceParse: 0
+    });
+	$('.form_time').datetimepicker({
+        language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 1,
+		minView: 0,
+		maxView: 1,
+		forceParse: 0
+    });
+</script>
+
+</body>
+</html>
