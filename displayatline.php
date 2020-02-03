93752c5e1d62bccae5e45da98cd2c7f692ff2773
prathan <than2557@gmail.com>
2020-02-03 10:13:20 +0700

    test

--- displayatline.php
+++ displayatline.php
@@ -221,8 +221,31 @@ function JSalert(){
 };
 $(document).ready(function() {
     $('#myTable').DataTable( {
-        "lengthMenu": [[3,10, 25, 50, -1], [3,10, 25, 50, "All"]]
+        "lengthMenu": [[3,10, 25, 50, -1], [3,10, 25, 50, "All"]],
+        "sEmptyTable":     "ไม่มีข้อมูลในตาราง",
+    "sInfo":           "แสดง _START_ ถึง _END_ จาก _TOTAL_ แถว",
+    "sInfoEmpty":      "แสดง 0 ถึง 0 จาก 0 แถว",
+    "sInfoFiltered":   "(กรองข้อมูล _MAX_ ทุกแถว)",
+    "sInfoPostFix":    "",
+    "sInfoThousands":  ",",
+    "sLengthMenu":     "แสดง _MENU_ แถว",
+    "sLoadingRecords": "กำลังโหลดข้อมูล...",
+    "sProcessing":     "กำลังดำเนินการ...",
+    "sSearch":         "ค้นหา: ",
+    "sZeroRecords":    "ไม่พบข้อมูล",
+    "oPaginate": {  
+        "sFirst":    "หน้าแรก",
+    "sPrevious": "ก่อนหน้า",
+        "sNext":     "ถัดไป",
+    "sLast":     "หน้าสุดท้าย"
+    },
+    "oAria": {
+        "sSortAscending":  ": เปิดใช้งานการเรียงข้อมูลจากน้อยไปมาก",
+    "sSortDescending": ": เปิดใช้งานการเรียงข้อมูลจากมากไปน้อย"
+    }
+        
     } );
+    
 } );
 
   </script>   
