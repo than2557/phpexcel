0fb1c8f52d827a1b1dbde7ca1e3b60224fd92f3b
Sabaho <powerganoflove@hotmail.com>
2020-02-07 14:00:36 +0700

    Auto stash before rebase of "origin/master"

--- select_ajax/condition_builder.js
+++ select_ajax/condition_builder.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(document).ready(function() {
 
     var i = 1;
@@ -102,3 +103,101 @@ var field = {
 
 
 };
+=======
+$(document).ready(function(){
+
+   var i=1;  
+   showjson();
+   //var table_fields ;
+
+   var html_table_fields;
+
+   if ($("#query").val() == null) {
+      alert("ไม่สามารถเรียกข้อมูลได้");
+  } 
+  else {
+      html_table_fields = populate_fields($("#query").val());
+      //console.log(html_table_fields)
+      //updateData($("#query").val());
+  }
+
+   $('#query').change(function(){
+      //console.log('table name onchange')
+   })
+
+   $('#add_condition').click(function(){  
+        var html  = '';
+         html += '<div class="row" style="margin-left:1%;margin-top:5px;" name="row'+i+'" id="row'+i+'">';
+         html += ' <div class="col-md-2"><select class="form-control oplist" name="oplist[]" > <option value="and">AND</option> <option value="or">OR</option></select></div>';
+         html += ' <div class="col-md-2"><select class="form-control fieldlist" name="fieldlist[]" >'+showjson()+'</select></div>';
+         html += '</div>';  
+ 
+        $('.append_condition').append(html);  
+
+        i++;  
+   }) 
+   
+   
+   $("#queryyyyy").click(function(){
+
+      $.ajax({
+         url: "select_ajax/get_condition_query.php", // test_json_encode.php เรียกข้อมูลจากฐานข้อมูลมาแสดงในรูปแบบ json
+         //url: "select_ajax/select_json_encode.php", // select dynamic field
+         method: "POST",
+         async: false,
+         dataType: "JSON",
+         data:  $('#get_query').serialize() ,
+         error: function(jqXHR, text, error) {
+             alert(error)
+         }
+     })
+     .done(function(data) {
+        console.log(data)
+     });
+   });
+})
+
+function populate_fields(table_name){
+
+   var response;
+
+   $.ajax({
+           url: "select_ajax/get_populate_fiels.php", // test_json_encode.php เรียกข้อมูลจากฐานข้อมูลมาแสดงในรูปแบบ json
+           //url: "select_ajax/select_json_encode.php", // select dynamic field
+           method: "POST",
+           async: false,
+
+           data: { table_name: table_name },
+           error: function(jqXHR, text, error) {
+               console.log(error)
+           }
+       })
+       .done(function(data) {
+           response = data;
+       });
+   return response;
+   return "<option>asdad</option>"
+}
+
+
+function showjson (){
+   var optionsss = '';
+   var data = {
+      VERSION: "2006-10-27.a",
+      "JOBNAME": "EXEC_",
+      "JOBHOST": "Test",
+      "LSFQUEUE": "45",
+      "LSFLIMIT": "2006-10-27",
+      "NEWUSER": "3",
+      "NEWGROUP": "2",
+      "NEWMODUS": "640"
+    };
+    
+    Object.keys(data).forEach(function(key) {
+      optionsss+='<option value="'+key+'">'+data[key]+'</option>';
+      
+    })
+    console.log(optionsss)
+    return optionsss;
+}
+>>>>>>> Stashed changes
