273d0097d1dc5484400effb38b65e09e34fca56e
prathan <than2557@gmail.com>
2020-02-07 15:52:48 +0700

    update

--- submit_file_new.php
+++ submit_file_new.php
@@ -69,6 +69,9 @@
       --color: hsl(210deg,10%,30%);
       background: #aee0ee;
     }
+
+
+    
   </style> 
 </head>
 <body>
@@ -76,6 +79,358 @@
       .modal_dialog_account_detail {max-height:100vh;max-width:80vw;}  
       .modal_body_account_detail{height:70vh;width:100%;align:center;}  
       .modal_container_account_detail{background-color:white;}    
+
+      .selectMultiple {
+  width: 150px;
+  position: relative;
+  margin-left:10px;
+}
+.selectMultiple select {
+  display: none;
+}
+.selectMultiple > div {
+  position: relative;
+  z-index: 2;
+  padding: 8px 12px 2px 12px;
+  border-radius: 8px;
+  background: #fff;
+  font-size: 14px;
+  min-height: 44px;
+  box-shadow: 0 4px 16px 0 rgba(22, 42, 90, 0.12);
+  -webkit-transition: box-shadow .3s ease;
+  transition: box-shadow .3s ease;
+}
+.selectMultiple > div:hover {
+  box-shadow: 0 4px 24px -1px rgba(22, 42, 90, 0.16);
+}
+.selectMultiple > div .arrow {
+  right: 1px;
+  top: 0;
+  bottom: 0;
+  cursor: pointer;
+  width: 28px;
+  position: absolute;
+}
+.selectMultiple > div .arrow:before, .selectMultiple > div .arrow:after {
+  content: '';
+  position: absolute;
+  display: block;
+  width: 2px;
+  height: 8px;
+  border-bottom: 8px solid #99A3BA;
+  top: 43%;
+  -webkit-transition: all .3s ease;
+  transition: all .3s ease;
+}
+.selectMultiple > div .arrow:before {
+  right: 12px;
+  -webkit-transform: rotate(-130deg);
+          transform: rotate(-130deg);
+}
+.selectMultiple > div .arrow:after {
+  left: 9px;
+  -webkit-transform: rotate(130deg);
+          transform: rotate(130deg);
+}
+.selectMultiple > div span {
+  color: #99A3BA;
+  display: block;
+  position: absolute;
+  left: 12px;
+  cursor: pointer;
+  top: 8px;
+  line-height: 28px;
+  -webkit-transition: all .3s ease;
+  transition: all .3s ease;
+}
+.selectMultiple > div span.hide {
+  opacity: 0;
+  visibility: hidden;
+  -webkit-transform: translate(-4px, 0);
+          transform: translate(-4px, 0);
+}
+.selectMultiple > div a {
+  position: relative;
+  padding: 0 24px 6px 8px;
+  line-height: 28px;
+  color: #1E2330;
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 6px 0 0;
+}
+.selectMultiple > div a em {
+  font-style: normal;
+  display: block;
+  white-space: nowrap;
+}
+.selectMultiple > div a:before {
+  content: '';
+  left: 0;
+  top: 0;
+  bottom: 6px;
+  width: 100%;
+  position: absolute;
+  display: block;
+  background: rgba(228, 236, 250, 0.7);
+  z-index: -1;
+  border-radius: 4px;
+}
+.selectMultiple > div a i {
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 24px;
+  height: 28px;
+  display: block;
+}
+.selectMultiple > div a i:before, .selectMultiple > div a i:after {
+  content: '';
+  display: block;
+  width: 2px;
+  height: 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  background: #4D18FF;
+  border-radius: 1px;
+}
+.selectMultiple > div a i:before {
+  -webkit-transform: translate(-50%, -50%) rotate(45deg);
+          transform: translate(-50%, -50%) rotate(45deg);
+}
+.selectMultiple > div a i:after {
+  -webkit-transform: translate(-50%, -50%) rotate(-45deg);
+          transform: translate(-50%, -50%) rotate(-45deg);
+}
+.selectMultiple > div a.notShown {
+  opacity: 0;
+  -webkit-transition: opacity .3s ease;
+  transition: opacity .3s ease;
+}
+.selectMultiple > div a.notShown:before {
+  width: 28px;
+  -webkit-transition: width 0.45s cubic-bezier(0.87, -0.41, 0.19, 1.44) 0.2s;
+  transition: width 0.45s cubic-bezier(0.87, -0.41, 0.19, 1.44) 0.2s;
+}
+.selectMultiple > div a.notShown i {
+  opacity: 0;
+  -webkit-transition: all .3s ease .3s;
+  transition: all .3s ease .3s;
+}
+.selectMultiple > div a.notShown em {
+  opacity: 0;
+  -webkit-transform: translate(-6px, 0);
+          transform: translate(-6px, 0);
+  -webkit-transition: all .4s ease .3s;
+  transition: all .4s ease .3s;
+}
+.selectMultiple > div a.notShown.shown {
+  opacity: 1;
+}
+.selectMultiple > div a.notShown.shown:before {
+  width: 100%;
+}
+.selectMultiple > div a.notShown.shown i {
+  opacity: 1;
+}
+.selectMultiple > div a.notShown.shown em {
+  opacity: 1;
+  -webkit-transform: translate(0, 0);
+          transform: translate(0, 0);
+}
+.selectMultiple > div a.remove:before {
+  width: 28px;
+  -webkit-transition: width 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44) 0s;
+  transition: width 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44) 0s;
+}
+.selectMultiple > div a.remove i {
+  opacity: 0;
+  -webkit-transition: all .3s ease 0s;
+  transition: all .3s ease 0s;
+}
+.selectMultiple > div a.remove em {
+  opacity: 0;
+  -webkit-transform: translate(-12px, 0);
+          transform: translate(-12px, 0);
+  -webkit-transition: all .4s ease 0s;
+  transition: all .4s ease 0s;
+}
+.selectMultiple > div a.remove.disappear {
+  opacity: 0;
+  -webkit-transition: opacity .5s ease 0s;
+  transition: opacity .5s ease 0s;
+}
+.selectMultiple > ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-size: 16px;
+  z-index: 1;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  visibility: hidden;
+  opacity: 0;
+  border-radius: 8px;
+  -webkit-transform: translate(0, 20px) scale(0.8);
+          transform: translate(0, 20px) scale(0.8);
+  -webkit-transform-origin: 0 0;
+          transform-origin: 0 0;
+  -webkit-filter: drop-shadow(0 12px 20px rgba(22, 42, 90, 0.08));
+          filter: drop-shadow(0 12px 20px rgba(22, 42, 90, 0.08));
+  -webkit-transition: all 0.4s ease, -webkit-transform 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44), -webkit-filter 0.3s ease 0.2s;
+  transition: all 0.4s ease, -webkit-transform 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44), -webkit-filter 0.3s ease 0.2s;
+  transition: all 0.4s ease, transform 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44), filter 0.3s ease 0.2s;
+  transition: all 0.4s ease, transform 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44), filter 0.3s ease 0.2s, -webkit-transform 0.4s cubic-bezier(0.87, -0.41, 0.19, 1.44), -webkit-filter 0.3s ease 0.2s;
+}
+.selectMultiple > ul li {
+  color: #1E2330;
+  background: #fff;
+  padding: 12px 16px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  -webkit-transition: background .3s ease, color .3s ease, opacity .5s ease .3s, border-radius .3s ease .3s, -webkit-transform .3s ease .3s;
+  transition: background .3s ease, color .3s ease, opacity .5s ease .3s, border-radius .3s ease .3s, -webkit-transform .3s ease .3s;
+  transition: background .3s ease, color .3s ease, transform .3s ease .3s, opacity .5s ease .3s, border-radius .3s ease .3s;
+  transition: background .3s ease, color .3s ease, transform .3s ease .3s, opacity .5s ease .3s, border-radius .3s ease .3s, -webkit-transform .3s ease .3s;
+}
+.selectMultiple > ul li:first-child {
+  border-radius: 8px 8px 0 0;
+}
+.selectMultiple > ul li:first-child:last-child {
+  border-radius: 8px;
+}
+.selectMultiple > ul li:last-child {
+  border-radius: 0 0 8px 8px;
+}
+.selectMultiple > ul li:last-child:first-child {
+  border-radius: 8px;
+}
+.selectMultiple > ul li:hover {
+  background: #4D18FF;
+  color: #fff;
+}
+.selectMultiple > ul li:after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 6px;
+  height: 6px;
+  background: rgba(0, 0, 0, 0.4);
+  opacity: 0;
+  border-radius: 100%;
+  -webkit-transform: scale(1, 1) translate(-50%, -50%);
+          transform: scale(1, 1) translate(-50%, -50%);
+  -webkit-transform-origin: 50% 50%;
+          transform-origin: 50% 50%;
+}
+.selectMultiple > ul li.beforeRemove {
+  border-radius: 0 0 8px 8px;
+}
+.selectMultiple > ul li.beforeRemove:first-child {
+  border-radius: 8px;
+}
+.selectMultiple > ul li.afterRemove {
+  border-radius: 8px 8px 0 0;
+}
+.selectMultiple > ul li.afterRemove:last-child {
+  border-radius: 8px;
+}
+.selectMultiple > ul li.remove {
+  -webkit-transform: scale(0);
+          transform: scale(0);
+  opacity: 0;
+}
+.selectMultiple > ul li.remove:after {
+  -webkit-animation: ripple .4s ease-out;
+          animation: ripple .4s ease-out;
+}
+.selectMultiple > ul li.notShown {
+  display: none;
+  -webkit-transform: scale(0);
+          transform: scale(0);
+  opacity: 0;
+  -webkit-transition: opacity .4s ease, -webkit-transform .35s ease;
+  transition: opacity .4s ease, -webkit-transform .35s ease;
+  transition: transform .35s ease, opacity .4s ease;
+  transition: transform .35s ease, opacity .4s ease, -webkit-transform .35s ease;
+}
+.selectMultiple > ul li.notShown.show {
+  -webkit-transform: scale(1);
+          transform: scale(1);
+  opacity: 1;
+}
+.selectMultiple.open > div {
+  box-shadow: 0 4px 20px -1px rgba(22, 42, 90, 0.12);
+}
+.selectMultiple.open > div .arrow:before {
+  -webkit-transform: rotate(-50deg);
+          transform: rotate(-50deg);
+}
+.selectMultiple.open > div .arrow:after {
+  -webkit-transform: rotate(50deg);
+          transform: rotate(50deg);
+}
+.selectMultiple.open > ul {
+  -webkit-transform: translate(0, 12px) scale(1);
+          transform: translate(0, 12px) scale(1);
+  opacity: 1;
+  visibility: visible;
+  -webkit-filter: drop-shadow(0 16px 24px rgba(22, 42, 90, 0.16));
+          filter: drop-shadow(0 16px 24px rgba(22, 42, 90, 0.16));
+}
+
+@-webkit-keyframes ripple {
+  0% {
+    -webkit-transform: scale(0, 0);
+            transform: scale(0, 0);
+    opacity: 1;
+  }
+  25% {
+    -webkit-transform: scale(30, 30);
+            transform: scale(30, 30);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    -webkit-transform: scale(50, 50);
+            transform: scale(50, 50);
+  }
+}
+
+@keyframes ripple {
+  0% {
+    -webkit-transform: scale(0, 0);
+            transform: scale(0, 0);
+    opacity: 1;
+  }
+  25% {
+    -webkit-transform: scale(30, 30);
+            transform: scale(30, 30);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    -webkit-transform: scale(50, 50);
+            transform: scale(50, 50);
+  }
+}
+
+
+* {
+  box-sizing: inherit;
+}
+*:before, *:after {
+  box-sizing: inherit;
+}
+
+
+
+
    </style>
 
    <!-- Modal -->
@@ -146,7 +501,7 @@
                                 $sql = "SELECT * FROM task_user WHERE user_id = '".$_SESSION["id_user"]."'";
                                 $result = mysqli_query($conn,$sql);
                             ?>  
-                            <select class="form-control col-md-2" name="query" id="query" style="width:300px;margin-left:30px;">
+                            <select class="form-control col-md-2"  multiple data-placeholder="ชื่อตาราง" name="query" id="query" style="width:300px;margin-left:30px;">
                                 <?php while($row = mysqli_fetch_array($result)){ 
                                     echo '<option value="'.$row[1].'">'.$row[1].'</option>'; 
                                 } ?> 
@@ -287,6 +642,106 @@
       maxView: 1,
       forceParse: 0
     });
+    $(document).ready(function() {
+
+var select = $('select[multiple]');
+var options = select.find('option');
+
+var div = $('<div />').addClass('selectMultiple');
+var active = $('<div />');
+var list = $('<ul />');
+var placeholder = select.data('placeholder');
+
+var span = $('<span />').text(placeholder).appendTo(active);
+
+options.each(function() {
+    var text = $(this).text();
+    if($(this).is(':selected')) {
+        active.append($('<a />').html('<em>' + text + '</em><i></i>'));
+        span.addClass('hide');
+    } else {
+        list.append($('<li />').html(text));
+    }
+});
+
+active.append($('<div />').addClass('arrow'));
+div.append(active).append(list);
+
+select.wrap(div);
+
+$(document).on('click', '.selectMultiple ul li', function(e) {
+    var select = $(this).parent().parent();
+    var li = $(this);
+    if(!select.hasClass('clicked')) {
+        select.addClass('clicked');
+        li.prev().addClass('beforeRemove');
+        li.next().addClass('afterRemove');
+        li.addClass('remove');
+        var a = $('<a />').addClass('notShown').html('<em>' + li.text() + '</em><i></i>').hide().appendTo(select.children('div'));
+        a.slideDown(400, function() {
+            setTimeout(function() {
+                a.addClass('shown');
+                select.children('div').children('span').addClass('hide');
+                select.find('option:contains(' + li.text() + ')').prop('selected', true);
+            }, 500);
+        });
+        setTimeout(function() {
+            if(li.prev().is(':last-child')) {
+                li.prev().removeClass('beforeRemove');
+            }
+            if(li.next().is(':first-child')) {
+                li.next().removeClass('afterRemove');
+            }
+            setTimeout(function() {
+                li.prev().removeClass('beforeRemove');
+                li.next().removeClass('afterRemove');
+            }, 200);
+
+            li.slideUp(400, function() {
+                li.remove();
+                select.removeClass('clicked');
+            });
+        }, 600);
+    }
+});
+
+$(document).on('click', '.selectMultiple > div a', function(e) {
+    var select = $(this).parent().parent();
+    var self = $(this);
+    self.removeClass().addClass('remove');
+    select.addClass('open');
+    setTimeout(function() {
+        self.addClass('disappear');
+        setTimeout(function() {
+            self.animate({
+                width: 0,
+                height: 0,
+                padding: 0,
+                margin: 0
+            }, 300, function() {
+                var li = $('<li />').text(self.children('em').text()).addClass('notShown').appendTo(select.find('ul'));
+                li.slideDown(400, function() {
+                    li.addClass('show');
+                    setTimeout(function() {
+                        select.find('option:contains(' + self.children('em').text() + ')').prop('selected', false);
+                        if(!select.find('option:selected').length) {
+                            select.children('div').children('span').removeClass('hide');
+                        }
+                        li.removeClass();
+                    }, 400);
+                });
+                self.remove();
+            })
+        }, 300);
+    }, 400);
+});
+
+$(document).on('click', '.selectMultiple > div .arrow, .selectMultiple > div span', function(e) {
+    $(this).parent().parent().toggleClass('open');
+});
+
+});
+
   </script>
   <script>
 
