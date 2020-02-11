cfc6bcbcb1a548f7ec7229a8a9fd2a74accd9c01
prathan <than2557@gmail.com>
2020-02-11 09:45:05 +0700

    psuh

--- select_ajax/get_condition_query.php
+++ select_ajax/get_condition_query.php
@@ -3,4 +3,10 @@
 echo $_POST['table_nameeeeeeeee'];
 // foreach($_POST as $post){
 //    var_dump($post);
+$oplist[] = $_POST['oplist'];
+$fieldlist[] = $_POST['fieldlist'];
+$condition_opv[] = $_POST['condition_opv'];
+// $valuelist[] = $_POST['valuelist'];
 // }
+print_r($oplist);
+?>
