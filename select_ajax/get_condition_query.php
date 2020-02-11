24ac102a6f3d9fc9d74b6276475a2daaa7b4b047
prathan <than2557@gmail.com>
2020-02-11 10:04:57 +0700

    push

--- select_ajax/get_condition_query.php
+++ select_ajax/get_condition_query.php
@@ -1,12 +1,18 @@
 <?php
+require('configDB.php');
 
-echo $_POST['table_nameeeeeeeee'];
+$table = $_POST['table_nameeeeeeeee'];
 // foreach($_POST as $post){
 //    var_dump($post);
 $oplist[] = $_POST['oplist'];
 $fieldlist[] = $_POST['fieldlist'];
 $condition_opv[] = $_POST['condition_opv'];
-// $valuelist[] = $_POST['valuelist'];
+
 // }
-print_r($oplist);
+ $arr = array('1'=>$oplist,'2'=>$fieldlist,'3'=>$condition_opv);
+ echo json_encode($arr[2]);
+//  $sql = "SELECT * FORM $table WHERE $arr[2]";
+//  echo $sql;
+//  $result=$conn->query($sql);
+
 ?>
