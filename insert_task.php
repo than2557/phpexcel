4ef85da5837df92daf5db2c6485aca6c8ceaf2b9
prathan <than2557@gmail.com>
2020-02-19 10:31:11 +0700

    update

--- insert_task.php
+++ insert_task.php
@@ -8,19 +8,28 @@ $user_id = $_POST['id_user'];
 
 $sql ="INSERT INTO `task_user`(`user_id`,`task_name`) VALUES ('$user_id','$task_name')";
     $Query = mysqli_query($conn,$sql);
-  
+    $task_user_id = mysqli_insert_id($conn);
+
+    
 $numrow = count($_POST['colum']);
 $row =$numrow-1;
 for ($i=0; $i <=$row;$i++) { 
    $datatype =$_POST['datatype'][$i];
    $colum =$_POST['colum'][$i];
-// print_r($datatype);
-// print_r($colum);
-$task_user_id = mysqli_insert_id($conn);
+
+
+echo $task_user_id;
 $sql2 = "INSERT INTO `template_tb`(`task_user_id`, `colum_name`,`datatype`) VALUES ($task_user_id,'$colum','$datatype')";
 $Query = mysqli_query($conn,$sql2);
 echo $sql2;
 }
+// for ($j=0; $j <=$row ; $j++) { 
+//     $datatype =$_POST['datatype'][$i];
+//     $colum =$_POST['colum'][$i]; 
+
+//     $headtb = 
+
+// }
 
 
 
