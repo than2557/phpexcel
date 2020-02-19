9247af6f4fedecbc6e309a4de98787d6434d0568
prathan <than2557@gmail.com>
2020-02-19 11:17:03 +0700

    update

--- insert_task.php
+++ insert_task.php
@@ -10,9 +10,10 @@ $sql ="INSERT INTO `task_user`(`user_id`,`task_name`) VALUES ('$user_id','$task_
     $Query = mysqli_query($conn,$sql);
     $task_user_id = mysqli_insert_id($conn);
 
-    
+
 $numrow = count($_POST['colum']);
 $row =$numrow-1;
+
 for ($i=0; $i <=$row;$i++) { 
    $datatype =$_POST['datatype'][$i];
    $colum =$_POST['colum'][$i];
@@ -23,13 +24,18 @@ $sql2 = "INSERT INTO `template_tb`(`task_user_id`, `colum_name`,`datatype`) VALU
 $Query = mysqli_query($conn,$sql2);
 echo $sql2;
 }
-// for ($j=0; $j <=$row ; $j++) { 
-//     $datatype =$_POST['datatype'][$i];
-//     $colum =$_POST['colum'][$i]; 
+$a = 1;
 
-//     $headtb = 
-
-// }
+for ($a;$a<=$row+1;$a++) {
+   $head = "h".$a.'<br>';
+   echo $head;
+$sql="CREATE TABLE $task_name (
+    `tb_id` int(11) NOT NULL,
+    `head` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+   
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+   
+}
 
 
 
