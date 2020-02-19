69b98e2da1fb7d0a941474eeedd3c5632f61acb8
prathan <than2557@gmail.com>
2020-02-19 10:10:05 +0700

    addtask

--- insert_task.php
+++ insert_task.php
@@ -6,24 +6,22 @@ $conn=$DBconnect;
 $task_name=$_POST['task_name'];
 $user_id = $_POST['id_user'];
 
-// $sql ="INSERT INTO `task_user`(`user_id`,`task_name`) VALUES ('$user_id','$task_name')";
-//     $Query = mysqli_query($conn,$sql);
-    $i=0;
-foreach($_POST['colum']as$vaulecol){
-
-$countcolum[$i] = count($vaulecol[$i]);
-$i++;
-print_r($countcolum);
-
-
+$sql ="INSERT INTO `task_user`(`user_id`,`task_name`) VALUES ('$user_id','$task_name')";
+    $Query = mysqli_query($conn,$sql);
+  
+$numrow = count($_POST['colum']);
+$row =$numrow-1;
+for ($i=0; $i <=$row;$i++) { 
+   $datatype =$_POST['datatype'][$i];
+   $colum =$_POST['colum'][$i];
+// print_r($datatype);
+// print_r($colum);
+$task_user_id = mysqli_insert_id($conn);
+$sql2 = "INSERT INTO `template_tb`(`task_user_id`, `colum_name`,`datatype`) VALUES ($task_user_id,'$colum','$datatype')";
+$Query = mysqli_query($conn,$sql2);
+echo $sql2;
 }
-for($i=0;$i<=$countcolum;$i++){
 
-    $datatype=$_POST['datatype'][$i];
-    $task_user_id = mysqli_insert_id($conn);
-    $sql2 = "INSERT INTO `template_tb`(`task_user_id`, `colum_name`,`datatype`) VALUES ($task_user_id,'$vaulecol','$datatype')";
-    $Query = mysqli_query($conn,$sql2);
-    echo $sql2;
-}
+
 
 ?>
